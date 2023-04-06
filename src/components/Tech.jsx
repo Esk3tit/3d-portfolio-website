330c0c7e275560464ff28dc730a6704251ed5d25
Khai Phan <khaiphn41@gmail.com>
2023-04-05 19:29:55 -0700

    Only modifying tech and ball models this time and leaving the other canvases alone... Getting closer but still having ref problems with drei Views it seems.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,25 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useRef } from "react";
 
 const Tech = () => {
+  const refs = useRef([]);
+  const containerRef = useRef();
+
+  refs.current.push(containerRef);
+
+  const handleRef = (ref) => {
+    refs.current.push(ref);
+    return ref;
+  }
+
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div ref={containerRef} className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
+        <div className="w-28 h-28" key={technology.name} onClick={() => console.log(refs)} ref={ref => handleRef(ref)} />
       ))}
+      <BallCanvas ref={refs} />
     </div>
   )
 }
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,9 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, forwardRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import { Decal, Float, OrbitControls, Preload, useTexture, View } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
+import { technologies } from '../../constants';
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -13,28 +14,46 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal position={[0, 0, 1]} rotation={[ 2 * Math.PI, 0, 6.25 ]} scale={1} flatShading map={decal} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          flatShading
+          map={decal}
+        />
       </mesh>
     </Float>
-  )
+  );
 }
 
-const BallCanvas = ({ icon }) => {
+const BallCanvas = forwardRef((_, refs) => {
+  console.log(refs.current)
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
+      eventSource={refs.current[0]}
     >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
+      {technologies.map((technology, index) => (
+        <View index={index + 1} track={refs.current[index + 1]}>
+          <Suspense fallback={<CanvasLoader />}>
+            <OrbitControls enableZoom={false} />
+            <Ball imgUrl={technology.icon} />
+          </Suspense>
+        </View>
+          
+      ))}
 
       <Preload all />
     </Canvas>
-  )
-}
+  );
+});
 
 export default BallCanvas;
