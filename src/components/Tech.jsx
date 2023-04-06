5c322cacaa643d761d572724e29fd77174454396
Khai Phan <khaiphn41@gmail.com>
2023-04-05 19:59:22 -0700

    Converted the skills balls to cards just like in the about section which frees up canvases and no longer causes webgl problems pog.
    Adjusted styles accordingly.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,15 +2,38 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { fadeIn, textVariant } from '../utils/motion';
+import { styles } from "../styles";
+
+const TechCard = ({ index, name, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div variants={fadeIn("up", "spring", 0.25 * index, 0.5)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div options={{ max: 45, scale: 1, speed: 800 }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[200px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={name} className='w-24 h-24 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{name}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>The Technologies I Know</p>
+        <h2 className={styles.sectionHeadText}>Skills.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology, index) => (
+          <TechCard key={technology.name} index={index} {...technology} />
+        ))}
+      </div>
+    </>
   )
 }
 
