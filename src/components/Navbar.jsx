efb923022772ab6eccddd7b18802a4db109dcfcb
Khai Phan <khaiphn41@gmail.com>
2023-04-07 17:49:05 -0700

    Mobile animation for tech cards still not working... Did fix some other stuff in navbar though.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,27 @@ import { logo, menu, close } from '../assets';
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${
+      scrolled ? "bg-primary" : "bg-transparent"
+    }`}>
       <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
         <Link
           to="/"
@@ -23,9 +41,10 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
             Khai &nbsp; 
-            <span className='sm:block hidden'>| Phan</span>
+            <span className='sm:block hidden'> | Phan</span>
           </p>
         </Link>
+
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
             <li key={link.id} 
@@ -41,7 +60,7 @@ const Navbar = () => {
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((link) => (
                 <li key={link.id} 
                   className={`${active === link.title ? "text-white" : "text-secondary"} 
@@ -62,4 +81,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,11 +7,12 @@ import { fadeIn, textVariant } from '../utils/motion';
 import { styles } from "../styles";
 
 const TechCard = ({ index, name, icon }) => {
+
   return (
     <Tilt className='xs:w-[225px] w-full'>
       <motion.div variants={fadeIn("up", "spring", 0.25 * index, 0.5)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        <div options={{ max: 45, scale: 1, speed: 450 }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[200px] flex justify-evenly items-center flex-col'>
-          <img src={icon} alt={name} className='w-28 h-28 object-contain' />
+        <div options={{ max: 45, scale: 1, speed: 450 }} className='bg-tertiary rounded-[20px] py-5 px-8 min-h-[200px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={name} className='w-16 h-16 object-contain' />
           <h3 className='text-white text-[20px] font-bold text-center'>{name}</h3>
         </div>
       </motion.div>
